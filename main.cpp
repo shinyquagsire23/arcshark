e39b48c5694fae338d8e4c0e34a15e773eeafb01
shinyquagsire23 <mtinc2@gmail.com>
2018-11-24 16:40:26 -0700

    parsing into offset 4

--- main.cpp
+++ main.cpp
@@ -26,6 +26,36 @@ typedef struct offset5_header
     uint32_t something2;
 } offset5_header;
 
+typedef struct offset4_header
+{
+    uint32_t total_size;
+    uint32_t unk2;
+    uint32_t unk3;
+    uint32_t entries;
+    
+    uint32_t entries_2;
+    uint32_t entries_3;
+    uint32_t unk6;
+    uint32_t entries_4;
+    uint32_t unk7;
+    uint32_t unk8;
+    uint32_t unk9;
+    uint32_t unk10;
+    uint32_t unk11;
+    uint32_t unk12;
+    uint32_t unk13;
+    uint32_t unk14;
+    uint32_t unk15;
+} offset4_header;
+
+typedef struct offset4_entry
+{
+    uint32_t a;
+    uint8_t b;
+    uint8_t c[3];
+    uint32_t d;
+} offset4_entry;
+
 typedef struct offset5_entry_1
 {
     uint32_t a;
@@ -44,7 +74,7 @@ std::map<uint32_t, std::string> unhash;
 
 int main(int argc, char** argv)
 {
-    if (argc < 3)
+    if (argc < 2)
     {
         printf("%s <data.arc>\n", argv[0]);
     }
@@ -73,12 +103,31 @@ int main(int argc, char** argv)
     printf("Offset 5: %016llx\n", header.offset_5);
     printf("Offset 6: %016llx\n\n", header.offset_6);
     
+    offset4_header off4_header;
+    fseek(f, header.offset_4, SEEK_SET);
+    fread(&off4_header, 0x44, 1, f);
+    
+    printf("Offset 4 Header:\n");
+    
+    void* off4_data = malloc(off4_header.total_size - 0x44);
+    fread(off4_data, off4_header.total_size - 0x44, 1, f);
+    
+    
+    offset4_entry* off4_entries = (offset4_entry*)off4_data;
+    for (int i = 0; i < 0x10; i++)
+    {
+        printf("%x: %08x %02x %02x%02x%02x %08x (%s)\n", i, off4_entries[i].a, off4_entries[i].b, off4_entries[i].c[2], off4_entries[i].c[1], off4_entries[i].c[0], off4_entries[i].d, unhash[off4_entries[i].a].c_str());
+    }
+    
+    
+    
+    
     offset5_header off5_header;
     fseek(f, header.offset_5, SEEK_SET);
     fread(&off5_header, 0x14, 1, f);
     
-    void* off5_data = malloc(off5_header.total_size);
-    fread(off5_data, off5_header.total_size, 1, f);
+    void* off5_data = malloc(off5_header.total_size - 0x14);
+    fread(off5_data, off5_header.total_size - 0x14, 1, f);
 
     printf("Offset 5 Header:\n");
     printf("Total size %016llx\n", off5_header.total_size);
@@ -87,7 +136,7 @@ int main(int argc, char** argv)
     
     printf("Hash table 1\n");
     offset5_entry_1* entries = (offset5_entry_1*)off5_data;
-    for (int i = 0; i < off5_header.entries; i++)
+    for (int i = 0; i < 0x40; i++)
     {
         //printf("%x: %08x %02x %02x%02x%02x (%s)\n", i, entries[i].a, entries[i].b, entries[i].c[2], entries[i].c[1], entries[i].c[0], unhash[entries[i].a].c_str());
     }
